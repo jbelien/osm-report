b17f8a22dbfc4bf9757c376e8c6098c8c223dfac
Jonathan Beliën <dev@jbelien.be>
2019-10-10 21:07:19 +0200

    Update popup.ts
    Display number of comments.

--- src/map/note/popup.ts
+++ src/map/note/popup.ts
@@ -6,7 +6,11 @@ export default function(feature: GeoJSON.Feature) {
   if (comments.length > 0) {
     const comment = comments[0].html;
 
-    return `${comment}<a target="_blank" href="https://www.openstreetmap.org/note/${id}" style="text-decoration: none; white-space: nowrap;"><i class="far fa-comments"></i> Read more...</a>`;
+    let content = `<p>${comment}</p>`;
+    content += `<p>${comments.length - 1} comment(s)</p>`;
+    content += `<a target="_blank" href="https://www.openstreetmap.org/note/${id}" style="text-decoration: none; white-space: nowrap;"><i class="far fa-comments"></i> Read more...</a>`;
+
+    return content;
   }
 
   return null;
