d3c15d588e269bb968f8b0fefd34ba50ae806e49
Jonathan Beliën <dev@jbelien.be>
2019-10-17 13:41:08 +0200

    Add note "age"
    Close #6

--- src/map/note/popup.ts
+++ src/map/note/popup.ts
@@ -1,12 +1,16 @@
 "use strict";
 
+import moment from "moment";
+
 export default function(feature: GeoJSON.Feature) {
-  const { id, comments } = feature.properties;
+  const { id, comments, date_created } = feature.properties;
 
   if (comments.length > 0) {
     const comment = comments[0].html;
+    const age = moment(date_created, "YYYY-MM-DD HH:mm:SS UTC", true).fromNow();
 
-    let content = `<p>${comment}</p>`;
+    let content = `<p><strong>Posted ${age}</strong></p>`;
+    content += `<p>${comment}</p>`;
     content += `<p>${comments.length - 1} comment(s)</p>`;
     content += `<a target="_blank" href="https://www.openstreetmap.org/note/${id}" style="text-decoration: none; white-space: nowrap;"><i class="far fa-comments"></i> Read more...</a>`;
 
